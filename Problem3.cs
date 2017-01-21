1a5bc87892b44f0c5b5e6062e8cdb2dbcfe7cbcc
Rud Miller <rud@combination.com>
2017-01-21 01:09:36 -0500

    Added Problem2 and Problem3. Added Utils partial class and Utils.Factor method.

---
+++ Problem3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    static class Problem3
+    {
+
+        //The prime factors of 13195 are 5, 7, 13 and 29.
+        //What is the largest prime factor of the number 600851475143 ?
+
+        static public int Run()
+        {
+            //60085147514311 prime
+            //60085147514312 isn't
+            long x = 60085147514312;
+
+            var factors = Utils.factor(x);
+
+            return factors.Last();   
+        }
+    }
+}
---
+++ Utils/Factor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    static partial class Utils
+    {
+        public static List<int> factor(long x)
+        {
+            List<int> factors = new List<int>();
+
+            for (long i = 2; i <= x; i++)
+            {
+                if (x % i == 0)
+                {
+                    factors.Add((int) i);
+                    x /= i;
+                }
+            }
+            return factors;
+        }
+    }
+}
