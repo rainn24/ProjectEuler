7d3cd07559547e8f880a77d9c67284f3e178b607
Rud Miller <rud@combination.com>
2017-01-31 14:31:20 -0500

    Added .vs/ProjectEuler/v14/.suo to ignore list. Added solution to Problem5.cs. Added Utils/Factors.cs which returns a list of all prime factors of a number including duplicates. Added Obsolete method attribute to Utils/Factor.cs just for kicks, it does fail in a few edge cases.

---
+++ Problem5.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    static class Problem5
+    {
+
+        //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+        //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+        static public int Run()
+        {
+
+            int answer = 1;
+            List<int> factorList = new List<int>();
+
+            for (int i = 2; i <= 20; i++)
+            {
+                if (i == 8)
+                {
+                    var a = 1;
+                }
+                var factors = Utils.factors(i);
+
+                for (int j = 0; j < factors.Count; j++)
+                {
+                    int factor = factors[j];    //a factor of the number i
+                    int factorCount = factors.Count(x => x == factor); //how many times this factor appears in the number i
+
+                    //check how often this factor appears in the overall list of factors
+                    //and compare it to how many times it appears in this number's factors
+                    int factorListCount = factorList.Count(x => x == factor);
+                    while (factorListCount < factorCount)
+                    {
+                        factorList.Add(factor);
+                        factorListCount++;
+                    }                                                            
+                }
+            }
+
+
+            foreach (int factor in factorList)
+            {
+                answer *= factor;
+            }
+            return answer;
+
+        }
+    }
+}
--- Utils/Factors.cs
+++ Utils/Factors.cs
@@ -8,16 +8,17 @@ namespace ProjectEuler
 {
     static partial class Utils
     {
-        [Obsolete("Use Utils.factors()")]
-        public static List<int> factor(long x)
+        public static List<int> factors(long x)
         {
+            //factor the number and return a list of all factors including multiples
+            //ex: factors(8) should return a list containing [2, 2, 2]
             List<int> factors = new List<int>();
 
             for (long i = 2; i <= x; i++)
             {
-                if (x % i == 0)
+                while (x % i == 0)
                 {
-                    factors.Add((int) i);
+                    factors.Add((int)i);
                     x /= i;
                 }
             }
